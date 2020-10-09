ed10b4fc2ee7ce37e9acee87a693232634d55d9d
Geni W <williamsgen@urbandaleschools.com>
2020-10-09 11:03:13 -0500

    practice ch 5

---
+++ Ch05_Practice.py
@@ -0,0 +1,100 @@
+
+# for i in range(10):
+#     i+=1
+#     print(i)
+
+# for i in range(1, 12, 2):
+#     print(i)
+
+# for i in range(10, -1, -1):
+#     print(i)
+# print("BLAST OFF!")
+
+# for i in range(3):
+#     print("a",end="")
+#     for j in range(3):
+#         print("b",end="")
+#     print()
+# print("Done")
+
+# total=0
+# for i in range(5):
+#     num=int(input("Please enter a number: "))
+#     total+=num
+# print("Your total is",total)
+
+# total=0
+# for i in range(5):
+#     new_number = int(input("enter a number: "))
+#     if new_number == 0:
+#         total += 1
+# print("you entered a total of", total, "zeros.")
+
+# a = 0
+# for i in range(10):
+#     a =+ 1
+# for i in range(10):
+#     a += 1
+# print(a)
+
+# a = 0
+# for i in range(10):
+#     a += 1
+#     for j in range(10):
+#         a += 1
+# print(a)
+
+# for i in range(10):
+#     print(i)
+#
+# i=0
+# while i<10:
+#     print(i)
+#     i+=1
+
+# i = 1
+# c=0
+# while i <= 2 ** 32:
+#     print(c,i)
+#     i *= 2
+#     c+=1
+
+# done=False
+# while not done:
+#     answer = input("If you want to quit, type y. ")
+#     if answer.lower()=='y':
+#         done="y"
+
+# import random
+# for i in range(5):
+#     my_number = random.randint(2, 51)
+#     print(my_number)
+
+# for letter in 'DEATH STAR':
+#     if letter == ' ':
+#         break
+#     print('Current Letter :', letter)
+
+# var = 10
+# while var > 0:
+#     print('Current variable value :', var)
+#     var = var -1
+#     if var == 5:
+#         break
+# print("Sorry, the Death Star shot early.")
+
+# var = 0
+# while var <= 10:
+#     var+=1
+#     if var%2 != 0:
+#         continue
+#     print('Current value:', var)
+# print("Goodbye!")
+
+# var = 0
+# while var <= 10:
+#     var += 1
+#     if var%2 != 0:
+#         pass
+#     print('Current value :', var)
+# print("Goodbye!")
