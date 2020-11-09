fad66d5bd7139525596cbbd3c07e37544aef1c85
Geni W <williamsgen@urbandaleschools.com>
2020-11-08 22:16:15 -0600

    Ch. 5 Roshambo

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -10,6 +10,60 @@ Add conditional statements to figure out who wins and keep the records
 When the user quits print a win/loss record
 
 '''
+done = False
+win = 0
+loss = 0
+import random
+while not done:
+    print('If you\'d like to quit enter y.')
+    quit = input("Do you want to quit? ")
+    if quit == "y":
+            done = True
+            print('Wins:', win)
+            print('Losses:', loss)
+    else:
+        print('Enter 1 for rock, 2 for paper, or 3 for scissors.')
+        choice = int(input("Please enter your choice: "))
+        computer = random.randint(1, 3)
+        if computer == 1:
+            print('The computer chose: rock')
+            if choice == 3:
+                loss+=1
+                print('Sorry, you lost this time.')
+                continue
+            elif choice == 2:
+                win+=1
+                print('Woohoo, you won!')
+                continue
+            else:
+                print('Tie!')
+                continue
+        elif computer == 2:
+            print('The computer chose: paper')
+            if choice == 1:
+                loss += 1
+                print('Sorry, you lost this time.')
+                continue
+            elif choice == 3:
+                win += 1
+                print('Woohoo, you won!')
+                continue
+            else:
+                print('Tie!')
+                continue
+        else:
+            print('The computer chose: scissors')
+            if choice == 2:
+                loss += 1
+                print('Sorry, you lost this time.')
+                continue
+            elif choice == 1:
+                win += 1
+                print('Woohoo, you won!')
+                continue
+            else:
+                print('Tie!')
+                continue
 
 
 
