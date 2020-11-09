2982983730e25753d1cc8a7b4108a09edb986f92
Geni W <williamsgen@urbandaleschools.com>
2020-11-08 21:17:55 -0600

    Ch. 5 Coin Toss

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,6 +6,19 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
+heads = 0
+tails = 0
+import random
+for i in range(50):
+      my_number = random.randint(0, 1)
+      if my_number == 0:
+          heads+=1
+          print('heads')
+      else:
+          tails+=1
+          print('tails')
+print('Total number of heads:', heads)
+print('Total number of tails:', tails)
 
 
 
